1cebc14202803a3fccc6d5c922a6de9d18eb1aaf
[sharoun <[madoyasharoun@gmail.com]>
2023-11-02 11:56:58 +0300

    :docs:folder Structure and Navbar,js starter

---
+++ client/src/components /NavBar.js
@@ -0,0 +1,18 @@
+import 'react'
+
+function navBar(){
+    return(
+        <>
+        <div></div>
+        <div>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+        </div>
+        </>
+    )
+}
