9c5964f2d60d33c727097000668848c4d1ac4a0f
[sharoun <[madoyasharoun@gmail.com]>
2023-11-02 13:19:09 +0300

    :art: feat:Navbar.js created navbar links

--- client/src/components /NavBar.js
+++ client/src/components /NavBar.js
@@ -1,18 +1,36 @@
-import 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
-function navBar(){
+
+
+
+function NavBar(){
+const home= 'Home'
+const about= 'About'
+const social='Social impact'
+const invest='Invest'
+const venture='Venture Labs'
+const brand='Brands'
+const careers='Careers'
+const contract='Contact'
+
     return(
-        <>
-        <div></div>
-        <div>
-            <a></a>
-            <a></a>
-            <a></a>
-            <a></a>
-            <a></a>
-            <a></a>
-            <a></a>
+        <div className='nav-container'>
+        <div className='logo'>
+            <img src='' alt='elewa-logo'></img>
         </div>
-        </>
+        <div className='nav-links'>
+        <Link exact to="/">{home}</Link>
+        <Link exact to="/">{about}</Link>
+        <Link exact to="/">{social}</Link>
+        <Link exact to="/">{invest}</Link>
+        <Link exact to="/">{venture}</Link>
+        <Link exact to="/">{brand}</Link>
+        <Link exact to="/">{careers}</Link>
+        <Link exact to="/">{contract}</Link>
+        </div>
+        </div>
     )
 }
+
+export default NavBar
