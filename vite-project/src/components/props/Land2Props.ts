edd157d36386373d141dd5d991457012c17c38c3
[sharoun <[madoyasharoun@gmail.com]>
2023-11-03 22:50:58 +0300

    :memo: wrote props for the 2nd landing page

---
+++ vite-project/src/components/props/Land2Props.ts
@@ -0,0 +1,19 @@
+//contains image and texts of the 2nd landing page of invest
+
+interface Land2Items{
+    src:string;
+    alt:string;
+    text:string;
+    header:string;
+}
+
+const Land2Page:Land2Items[]=[
+    {
+    src:"vite-project/src/assets/images/Mask_Group_17.png",
+    alt: "Elewa",
+    text: "The global South is rapidly catching up with the developed world. It's time for you to get to know the rapid development, innovation and drive going on in this part of the world (East-Africa, South Asia and beyond).",
+    header: "How we see it differently",
+    },
+]
+
+export default Land2Page
