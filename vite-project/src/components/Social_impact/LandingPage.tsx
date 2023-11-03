b9c0adde1e84effda3838a23daadc1fe82cefe32
[sharoun <[madoyasharoun@gmail.com]>
2023-11-03 21:29:49 +0300

    :construction:Mapped landing page images and texts

--- vite-project/src/components/Social_impact/LandingPage.tsx
+++ vite-project/src/components/Social_impact/LandingPage.tsx
@@ -1,11 +1,18 @@
+//it displays the landing page Text, image and title
+
 import LandingPage from "../props/LandingPageprops";
 
 
 function LandingPg(){
     return(
-        <div>
-            {/* <h1>{LandingPage.title}</h1> */}
-
+        <div className="landing-section">
+        {LandingPage.map((item, index) => (
+            <div key={index}>
+                <img src={item.src} alt={item.alt} />
+                <h2>{item.text}</h2>
+                <h1>{item.header}</h1>
+            </div>
+        ))}
         </div>
     )
 }
