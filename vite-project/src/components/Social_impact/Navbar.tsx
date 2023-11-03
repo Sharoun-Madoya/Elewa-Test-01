77a34eff286a6ffd89bee5624080bc7bd80abaf5
[sharoun <[madoyasharoun@gmail.com]>
2023-11-03 11:57:57 +0300

    :doc: mapped the header props

--- vite-project/src/components/Social_impact/Navbar.tsx
+++ vite-project/src/components/Social_impact/Navbar.tsx
@@ -1,14 +1,22 @@
+//lists and displays the Header items onto the navbar
+
+import Header from "../props/header"
+
 function NavBar(){
-    const home= 'Home'
-    const about= 'About'
-    const social='Social impact'
-    const invest='Invest'
-    const venture='Venture Labs'
-    const brand='Brands'
-    const careers='Careers'
-    const contract='Contact'
 return(
-    <>
-    <div></div>
-    </>
-)}
+    <div className=".navContainer">
+        <div className= 'container'>
+            <img src='' alt='elewa-logo' ></img>
+            <div>
+                {Header.map(headerItem =>(
+                    <a key={headerItem.path} href={"headerItem.path"}>
+                        {headerItem.headerText}
+                    </a>
+                ))}
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default NavBar;
---
+++ vite-project/src/components/props/header.ts
@@ -0,0 +1,50 @@
+// defining the interface objects for the headerItems
+interface HeaderItems{
+    headerText: string;
+    path:string
+}
+
+const Header:HeaderItems[]=[
+    {
+        headerText:"Home",
+        path:"/"
+
+    },
+    {
+        headerText:"About",
+        path:"/About"
+
+    },
+    {
+        headerText:"Social impact",
+        path:"/Social impact"
+
+    },
+    {
+        headerText:"Invest",
+        path:"/Invest"
+
+    },
+    {
+        headerText:"Venture Labs",
+        path:"/Venture Labs"
+
+    },
+    {
+        headerText:"Brands",
+        path:"/Brands"
+
+    },
+    {
+        headerText:"Careers",
+        path:"/Careers"
+
+    },
+    {
+        headerText:"Contact",
+        path:"/Contact"
+
+    }
+]
+
+export default Header
