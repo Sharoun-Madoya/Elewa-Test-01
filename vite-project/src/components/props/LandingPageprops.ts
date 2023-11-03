540d012453345a3c46bdf252e9eb9cb6a95a1824
[sharoun <[madoyasharoun@gmail.com]>
2023-11-03 20:53:14 +0300

    :tada: created landing page props

---
+++ vite-project/src/components/props/LandingPageprops.ts
@@ -0,0 +1,19 @@
+//contains image and texts of the landing page of invest
+
+interface LandingItems{
+    src:string;
+    alt:string;
+    text:string;
+    header:string;
+}
+
+const LandingPage:LandingItems[]=[
+    {
+    src:"vite-project/src/assets/images/Logo_White_arvoyx.svg",
+    alt: "Elewa",
+    text: "Trade, not aid",
+    header: "Southern innovations addressing global problems",
+    },
+]
+
+export default LandingPage
---
+++ vite-project/src/components/props/headerImgProps.ts
@@ -0,0 +1,16 @@
+//holds the elewa image logo
+
+interface ElewaLogo{
+    src: string;
+    alt:string
+}
+
+
+const Logo:ElewaLogo[]=[
+    {
+    src:"/vite-project/src/assets/images/Logo_White_arvoyx.svg",
+    alt: "Elewa-logo",
+    },
+]
+
+export default Logo
