c174b9c4194edda1c49b4769c0cb35cbe90e6891
[sharoun <[madoyasharoun@gmail.com]>
2023-11-03 13:46:02 +0300

    :art: displaying footer

--- vite-project/src/components/Social_impact/Footer.tsx
+++ vite-project/src/components/Social_impact/Footer.tsx
@@ -1,57 +1,66 @@
-import Headquaters from "../props/HeadquatersProps"
-import Navigation from "../props/navigationprops"
-import Brands from "../props/brandprops"
-import Contacts from "../props/contactprops"
-import Privacy from "../props/Privacyprops"
+import React from 'react';
+import Headquaters from "../props/HeadquatersProps";
+import Navigation from "../props/navigationprops";
+import Brands from "../props/brandprops";
+import Contacts from "../props/contactprops";
+import Privacy from "../props/Privacyprops";
 
-function footer(){
-    return(
+function Footer() {
+    return (
         <div className="footerPage">
             <div className="footerContainer">
                 <hr></hr>
                 <div>
-                    <li>
-                    {Headquaters.map(headerItem =>(
-                    <a key={headerItem.path} href={"headerItem.path"}>
-                        {headerItem.headerText}
-                    </a>
-                ))}
-                    </li>
-                    <li>
-                    {Navigation.map(headerItem =>(
-                    <a key={headerItem.path} href={"headerItem.path"}>
-                        {headerItem.headerText}
-                    </a>
-                ))}
-                    </li>
-                    <li>
-                    {Brands.map(headerItem =>(
-                    <a key={headerItem.path} href={"headerItem.path"}>
-                        {headerItem.headerText}
-                    </a>
-                ))}
-                    </li>
-                    <li>{Contacts.map(headerItem =>(
-                    <a key={headerItem.path} href={"headerItem.path"}>
-                        {headerItem.headerText}
-                    </a>
-                ))}
-                </li>
-                <li>
-                {Privacy.map(headerItem =>(
-                    <a key={headerItem.path} href={"headerItem.path"}>
-                        {headerItem.headerText}
-                    </a>
-                ))}
-                </li>
+                    <ul>
+                        {Headquaters.map(headquarterItem => (
+                            <li key={headquarterItem.town}>
+                                <h3>{headquarterItem.footerHeader}</h3>
+                                <p>
+                                    {headquarterItem.building}, {headquarterItem.road}, {headquarterItem.town}
+                                </p>
+                            </li>
+                        ))}
+                    </ul>
+                    <ul>
+                        {Object.values(Navigation).map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                    <ul>
+                        {Contacts.map(contact => (
+                            <li key={contact.number}>
+                                <p>{contact.number}</p>
+                                <p>{contact.email}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <ul>
+                        {Brands.map(brand => (
+                            <li key={brand.elewa}>
+                                <p>{brand.elewa}</p>
+                                <p>{brand.italanta}</p>
+                                <p>{brand.labs}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <ul>
+                        {Privacy.map(privacy => (
+                            <li key={privacy.terms}>
+                                <h3>Terms and Conditions</h3>
+                                <p>{privacy.terms}</p>
+                                <h3>Cookie Preferences</h3>
+                                <p>{privacy.cookie}</p>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
                 <div>
-                    <img></img>
+                    <img src="" alt="Elewa Logo"></img>
                     <ul></ul>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default footer
+export default Footer;
