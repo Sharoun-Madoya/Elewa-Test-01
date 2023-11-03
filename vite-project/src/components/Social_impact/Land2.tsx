f83a9c618010665cf179b83a5fc603b85d4a9c54
[sharoun <[madoyasharoun@gmail.com]>
2023-11-03 22:56:36 +0300

    :memo: Mapped for the 2nd landing page

---
+++ vite-project/src/components/Social_impact/Land2.tsx
@@ -0,0 +1,19 @@
+
+
+import Land2Page from "../props/Land2Props"
+
+function Land2Pg(){
+    return(
+        <div className="land2-section">
+        {Land2Page.map((item, index) => (
+            <div key={index}>
+                <h5 id="page2-text">{item.header}</h5><br></br>
+                <h4 id="page2-text">{item.text}</h4>
+                <img src={item.src} alt={item.alt} />
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default Land2Pg
