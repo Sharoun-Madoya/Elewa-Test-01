f461996fcbf20e8a8f6d2fa39ff40a9d8565e878
[sharoun <[madoyasharoun@gmail.com]>
2023-11-04 21:10:14 +0300

    :memo: Codded App basics

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,13 +1,21 @@
 
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Invest from './components/sections/Invest'
 
 
 function App() {
   return (
-    <div>
-    <Invest/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/invest" element={<Invest />} />
+          <Route path="/" element={<SocialInvest />} />
+        </Routes>
+      </div>
+    </Router>
+
   )
 }
 
