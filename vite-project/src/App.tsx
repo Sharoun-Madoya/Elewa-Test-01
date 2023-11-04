b9869718243ef70b184a213bb6087c3d6c081cbb
[sharoun <[madoyasharoun@gmail.com]>
2023-11-04 21:05:26 +0300

    :fire: Removed unnecessary code from App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,20 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './components/Views/Navbar'
-import Footer from './components/Views/Footer'
-import LandingPg from './components/Views/LandingPage'
-import Land2Pg from './components/Views/Land2'
 
+import './App.css'
+import Invest from './components/sections/Invest'
 
 
 function App() {
   return (
     <div>
-    <NavBar/>
-    <LandingPg/><br></br>
-    <Land2Pg/>
+    <Invest/>
     </div>
   )
 }
