cb05c9125c43899aad1d11c5d336e538d2138673
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 17:08:28 +0300

    :art: feat: Coded page 6 view model

---
+++ vite-project/src/components/ViewModels/usePage6Data.ts
@@ -0,0 +1,19 @@
+import { Page6Items } from "../modelsData/Page6Props";
+import { useEffect, useState } from 'react';
+
+
+
+export const usePage6Data = () => {
+    const [page6, setPage6] = useState<Page6Items[]>([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:3001/page6')
+        .then((response) => response.json())
+        .then((data) => {
+          setPage6(data);
+          console.log(data);
+        });
+    }, []);
+  
+    return page6
+}
