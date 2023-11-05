0601fe45c9b6575546f921906a72518443a2a7df
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 21:07:47 +0300

    :memo: Coded for views social impact landing 4 page

---
+++ vite-project/src/components/Views/socialViews/socialLand4page.tsx
@@ -0,0 +1,21 @@
+import { useSLand4Page } from "../../ViewModels/socialViewModels/useSLand4Page";
+
+function SocialPg4(){
+const SPage4= useSLand4Page();
+
+    return(
+        <div className="p-section">
+        {SPage4.map((item, index) => (
+            <div key={index}>
+                <img src={item.src} alt={item.alt} />
+                <h1>{item.title}</h1>
+                <h4 id="page-text">{item.text1}</h4>
+                <h4 id="page-text">{item.text2}</h4>
+                <h4 id="page-text">{item.text3}</h4>
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default SocialPg4
