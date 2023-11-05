680bdf195113b71c4f2b5566efbb5321e59d15f1
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 21:03:28 +0300

    :memo: Coded for views social impact landing 3 page

---
+++ vite-project/src/components/Views/socialViews/socialLand3page.tsx
@@ -0,0 +1,19 @@
+import { useSLand3Page } from "../../ViewModels/socialViewModels/useSLand3Page";
+
+function SocialPg3(){
+const SPage3= useSLand3Page();
+
+    return(
+        <div className="p-section">
+        {SPage3.map((item, index) => (
+            <div key={index}>
+                <img src={item.src} alt={item.alt} />
+                <h1>{item.title}</h1>
+                <h4 id="page-text">{item.text1}</h4>
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default SocialPg3
