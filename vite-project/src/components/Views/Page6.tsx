afaaa602754864296c5023088bfd8b66b0e5d59a
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 17:22:57 +0300

    :memo: feat: Coded page 6 view

---
+++ vite-project/src/components/Views/Page6.tsx
@@ -0,0 +1,20 @@
+import { usePage6Data } from "../ViewModels/usePage6Data";
+
+function Pg6(){
+const Page6Pg6= usePage6Data();
+
+    return(
+        <div className="p-section">
+        {Page6Pg6.map((item, index) => (
+            <div key={index}>
+                <img src={item.src} alt={item.alt} />
+                <h1>{item.title}</h1>
+                <h5 id="page-text">{item.text2}</h5>
+                <h4 id="page-text">{item.text1}</h4>
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default Pg6
