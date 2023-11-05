97e50ec5a78a4d197810eb2c8cfc5f82c1752a94
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 17:10:06 +0300

    :art: feat: Coded page 7 view model

---
+++ vite-project/src/components/ViewModels/usePage7Data.ts
@@ -0,0 +1,19 @@
+import { Page7Items } from "../modelsData/Page7Props";
+import { useEffect, useState } from 'react';
+
+
+
+export const usePage7Data = () => {
+    const [page7, setPage7] = useState<Page7Items[]>([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:3001/page7')
+        .then((response) => response.json())
+        .then((data) => {
+          setPage7(data);
+          console.log(data);
+        });
+    }, []);
+  
+    return page7
+}
