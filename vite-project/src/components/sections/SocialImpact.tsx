b4cff25b63ebe4ee18bd63fabee27785c6d4abbb
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 21:16:38 +0300

    :rocket: Rendered Social Impact Page

--- vite-project/src/components/sections/SocialImpact.tsx
+++ vite-project/src/components/sections/SocialImpact.tsx
@@ -1,13 +1,20 @@
 import NavBar from '../Views/Navbar'
 import Footer from '../Views/Footer'
 import SocialLandingPg from '../Views/socialViews/SocialLandingPage'
-
+import SocialLand2Pg from '../Views/socialViews/socialLand2page'
+import SocialPg3 from '../Views/socialViews/socialLand3page'
+import SocialPg4 from '../Views/socialViews/socialLand4page'
+import SocialPg5 from '../Views/socialViews/socialLand5page'
 
 function SocialImpact(){
     return(
         <div>
             <NavBar/>
             <SocialLandingPg/>
+            <SocialLand2Pg/>
+            <SocialPg3/>
+            <SocialPg4/>
+            <SocialPg5/>
             <Footer/>
         </div>
     )
