6161936aa26cc07c05adef06b78cc69329827aad
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 20:06:22 +0300

    :memo: Feat: coded the viewModel for social Landing 2 page

---
+++ vite-project/src/components/ViewModels/socialViewModels/useSLand2Page.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { sLand2Items } from '../../modelsData/socialModels/s-Landing2Props';
+
+
+export const useSocialLand2Page = () => {
+    const [sLand2Page, setSLand2Page] = useState<sLand2Items[]>([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:3001/social-l')
+        .then((response) => response.json())
+        .then((data) => {
+          setSLand2Page(data);
+          console.log(data);
+        });
+    }, []);
+  
+    return sLand2Page
+}
