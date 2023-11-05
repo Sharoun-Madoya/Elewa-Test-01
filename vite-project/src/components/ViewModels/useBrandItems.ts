e9073440e0743b16a049bf50d4ba7a30271b1f44
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 14:14:31 +0300

    :memo: Coded useBrandItems

---
+++ vite-project/src/components/ViewModels/useBrandItems.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { BrandsItems } from '../modelsData/brandprops';
+
+
+
+export const usePrivacy= () => {
+    const [brandItems, setBrandItems] = useState<BrandsItems[]>([]);
+
+  
+    useEffect(() => {
+      fetch('http://localhost:3001/footer/brands')
+        .then((response) => response.json())
+        .then((data) => {
+          setBrandItems(data);
+          console.log(data);
+        });
+    }, []);
+  
+    return brandItems
+}
