8148d3e1e61bce95d3c9a1328bca5a1023a94a5b
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 12:33:18 +0300

    :recycle: Recycled logo into json

--- vite-project/src/components/modelsData/headerImgProps.ts
+++ vite-project/src/components/modelsData/headerImgProps.ts
@@ -5,12 +5,3 @@ interface ElewaLogo{
     alt:string
 }
 
-
-const Logo:ElewaLogo[]=[
-    {
-    src:"https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg",
-    alt: "elewa-logo",
-    },
-]
-
-export default Logo
