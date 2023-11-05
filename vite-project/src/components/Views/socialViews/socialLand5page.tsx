9949d3095cc091519d3c291ca8df679991538c50
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 21:11:30 +0300

    :memo: Coded for views social impact landing 5 page

---
+++ vite-project/src/components/Views/socialViews/socialLand5page.tsx
@@ -0,0 +1,20 @@
+import { useSLand5Page } from "../../ViewModels/socialViewModels/useSLand5Page";
+
+function SocialPg5(){
+const SPage5= useSLand5Page();
+
+    return(
+        <div className="p-section">
+        {SPage5.map((item, index) => (
+            <div key={index}>
+                <img src={item.src} alt={item.alt} />
+                <h1>{item.title}</h1>
+                <h4 id="page-text">{item.text1}</h4>
+                <h4 id="page-text">{item.text2}</h4>
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default SocialPg5
