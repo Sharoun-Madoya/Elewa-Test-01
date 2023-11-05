1ee6304f4ed4771af383b26c48797aba0d27848e
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 17:20:31 +0300

    :memo: feat: Coded page 5 view

---
+++ vite-project/src/components/Views/Page5.tsx
@@ -0,0 +1,20 @@
+import { usePage5Data } from "../ViewModels/usePage5Data";
+
+function Pg5(){
+const Page5= usePage5Data();
+
+    return(
+        <div className="p-section">
+        {Page5.map((item, index) => (
+            <div key={index}>
+                <h1>{item.title}</h1>
+                <h5 id="page-text">{item.text2}</h5>
+                <h5 id="page-text">{item.text1}</h5>
+                <img src={item.src} alt={item.alt} />
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default Pg5
