89b33984356e3a749d1e86fcabe0ad7f9633f781
[sharoun <[madoyasharoun@gmail.com]>
2023-11-05 14:24:25 +0300

    :art:created use icon data view model

---
+++ vite-project/src/components/ViewModels/useIconsData.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { IconLogo } from '../modelsData/iconProps';
+
+
+export const useIconData = () => {
+    const [iconLogo, setIconLogo] = useState<IconLogo[]>([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:3001/footer/icons')
+        .then((response) => response.json())
+        .then((data) => {
+          setIconLogo(data);
+          console.log(data);
+        });
+    }, []);
+  
+    return iconLogo
+}
